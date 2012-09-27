8be85e85484f3e40daf9935d50fe030e8da7b8c8
Joachim Kainz <jfk@jolira.com>
2012-09-27 15:07:03 -0700

    small fixes for production

--- bin/config.sh
+++ bin/config.sh
@@ -8,7 +8,8 @@ fi
 apt-get -y install python-software-properties
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
-apt-get -y install nodejs-dev npm git
+apt-get -y install nodejs-dev npm git build-essential
+apt-get -y install libavahi-compat-libdnssd-dev libexpat1-dev
 adduser --system --no-create-home --disabled-password deployer
 cat << EOF > /etc/init/deployer.conf
 # start the deployer
